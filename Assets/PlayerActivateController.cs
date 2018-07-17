6420892ee023b814bb7c2c74379eed233321fd76
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2018-07-17 10:51:47 +1000

    Implement Player script detecting activable objects are in range

---
+++ Assets/PlayerActivateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActivateController : MonoBehaviour {
+
+	public List<GameObject> activableGameObjectsInRange;
+
+	// Use this for initialization
+	void Start () {
+		activableGameObjectsInRange = new List<GameObject>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Activable" && !activableGameObjectsInRange.Contains(other.gameObject)) {
+			activableGameObjectsInRange.Add(other.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Activable") {
+			activableGameObjectsInRange.Remove(other.gameObject);
+		}
+	}
+}
